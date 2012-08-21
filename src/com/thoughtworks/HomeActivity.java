91ff501f4e909a44657aa0a51ea85eef6eb41077
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2012-08-21 22:40:14 +0530

    Adding init directory structure

---
+++ src/com/thoughtworks/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomeActivity extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

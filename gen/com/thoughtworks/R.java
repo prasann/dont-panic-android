4693bc8be40d4afc2eb4b5bab74150011cc21d31
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2012-08-22 23:06:35 +0530

    Added button styles to the app

--- gen/com/thoughtworks/R.java
+++ gen/com/thoughtworks/R.java
@@ -10,13 +10,27 @@ package com.thoughtworks;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int buttons=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+    }
+    public static final class id {
+        public static final int TextView01=0x7f070000;
+        public static final int view_all=0x7f070001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int home=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
+        public static final int view_all=0x7f050001;
+    }
+    public static final class style {
+        public static final int MyTheme=0x7f060000;
+        public static final int custom_button=0x7f060001;
     }
 }
